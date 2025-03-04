0bbdb602b3761405ac5da254817a34e538552c6d
Ash-ishkushwaha <ashishkushonlyone1@gmail.com>
2025-03-04 12:11:18 +0530

    UC3 added param request

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,29 @@
-package com.example.demo;
+package com.example.demo.Service;
+import org.springframework.stereotype.Service;
+@Service
+public class GreetingService {
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class DemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+	public String getGreeting(String firstName, String lastName) {
+		if (firstName != null && lastName != null) {
+			return "{\"message\": \"Hello, " + firstName + " " + lastName + "!\"}";
+		} else if (firstName != null) {
+			return "{\"message\": \"Hello, " + firstName + "!\"}";
+		} else if (lastName != null) {
+			return "{\"message\": \"Hello, " + lastName + "!\"}";
+		} else {
+			return "{\"message\": \"Hello, World!\"}";
+		}
 	}
 
+	public String createGreeting() {
+		return "{\"message\": \"Greeting Created!\"}";
+	}
+
+	public String updateGreeting() {
+		return "{\"message\": \"Greeting Updated!\"}";
+	}
+
+	public String deleteGreeting() {
+		return "{\"message\": \"Greeting Deleted!\"}";
+	}
 }
